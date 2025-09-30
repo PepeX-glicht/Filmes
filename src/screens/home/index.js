36dae39d34593b6e46f93f571666f85f35be710e
PepeX-glicht <pedro.conceicao5309@gmail.com>
2025-09-30 03:24:17 -0300

    Adicionado o segundo carrossel

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,68 +1,104 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList } from "react-native";
-import BannerFilmes from '../../components/bannerFilmes'
-import dados from "../../components/data/data.js";
+import { StyleSheet, View, FlatList, ScrollView } from "react-native";
 import Header from "../../components/header";
 import Search from "../../components/searchbar";
+import BannerFilmes from "../../components/bannerFilmes";
 import CardMovies from "../../components/cardMovies";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export default function Home(){
+export default function Home() {
+  const [moviesPopular, setMoviesPopular] = useState([]);
+  const [moviesTopRated, setMoviesTopRated] = useState([]);
 
-const [movies, setMovies] = useState([]);
-useEffect(()=> {
-async function buscarFilmes(){
-const url = 'https://api.themoviedb.org/3/movie/now_playing?language=pt-BR-US&page=1';
-const options = {
-method: 'GET',
-headers: {
-accept: 'application/json',
-Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMDMzZDJjYzk5Mjc1MWRjN2RhZTg3YTYyOTM5ZjAyNCIsIm5iZiI6MTc1NjIyNjY0MS4yNjgsInN1YiI6IjY4YWRlNDUxY2JmNzM4YTZhOTFlNWFjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2-odY0qSgxsJoUWWtCfm9lJ52Xp3M6Dta70AMzYfj0M'
-}}
+  useEffect(() => {
+    async function fetchMovies() {
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMDMzZDJjYzk5Mjc1MWRjN2RhZTg3YTYyOTM5ZjAyNCIsIm5iZiI6MTc1NjIyNjY0MS4yNjgsInN1YiI6IjY4YWRlNDUxY2JmNzM4YTZhOTFlNWFjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2-odY0qSgxsJoUWWtCfm9lJ52Xp3M6Dta70AMzYfj0M",
+        },
+      };
 
-const response = await fetch(url,options);
-const data = await response.json();
+      try {
+        const [popularResponse, topRatedResponse] = await Promise.all([
+          fetch(
+            "https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1",
+            options
+          ),
+          fetch(
+            "https://api.themoviedb.org/3/movie/top_rated?language=pt-BR&page=1",
+            options
+          ),
+        ]);
 
-console.log(data);
-setMovies(data.results);
+        const popularData = await popularResponse.json();
+        const topRatedData = await topRatedResponse.json();
+
+        setMoviesPopular(popularData.results.slice(0, 15));
+        setMoviesTopRated(topRatedData.results.slice(0, 15));
+      } catch (error) {
+        console.error("Erro ao buscar filmes:", error);
+      }
+    }
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <ScrollView style={styles.container} contentContainerStyle={{ alignItems: "center", paddingBottom: 50 }}>
+      <StatusBar style="light" />
+      <Header />
+      <Search />
+      <BannerFilmes />
+
+      
+      <View style={styles.carouselContainer}>
+        <FlatList
+          data={moviesPopular}
+          horizontal
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <CardMovies
+              titulo={item.title}
+              nota={item.vote_average}
+              img={item.poster_path}
+              sinopse={item.overview}
+            />
+          )}
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
+
+     
+      <View style={styles.carouselContainer}>
+        <FlatList
+          data={moviesTopRated}
+          horizontal
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <CardMovies
+              titulo={item.title}
+              nota={item.vote_average}
+              img={item.poster_path}
+              sinopse={item.overview}
+            />
+          )}
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
+    </ScrollView>
+  );
 }
 
-buscarFilmes();
-
-}, [])
-
-return (
-<View style={style.container}>
-<Header></Header>
-<Search></Search>
-<BannerFilmes></BannerFilmes>
-
-<View style={{width:'90%'}}>
-<FlatList
-data = {movies} //trocamos por "dados" 
-horizontal = {true} 
-keyExtractor={(item) => item.id}
-renderItem= {({item}) => (
-
-<CardMovies 
-titulo = {item.title}
-nota = {item.vote_average}  
-img = {item.poster_path} 
-sinopse = {item.overview} >
-</CardMovies>
-
-)}
-/>
-</View>
-</View>
-)}
- 
-const style = StyleSheet.create({
-
-container: {
-   flex: 1,
-   backgroundColor: '#141a29',
-   alignItems:"center"
-}
-
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#141a29",
+  },
+  carouselContainer: {
+    width: "90%",
+    marginTop: 20,
+  },
+});
